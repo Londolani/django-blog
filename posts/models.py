b7787a368613a5518f11bae5dddea1b4c4acc6ac
Londolani <londolani0700@gmail.com>
2022-05-11 00:53:23 +0200

    added model and linked templates

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
+from ast import mod
+from turtle import title
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    
+
